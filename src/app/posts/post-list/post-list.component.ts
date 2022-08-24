25d851a04544d7e436b6119a5f5d37c0d3d124e9
Brant Hajek <branthajek@gmail.com>
2022-08-23 21:18:34 -0700

    first commit

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  private postSubcription: Subscription;
+
+  constructor(public postsService: PostsService) { }
+
+  ngOnInit(): void {
+    this.postsService.getPosts();
+    this.postSubcription = this.postsService.getPostUpdateListener().subscribe(
+      (posts: Post[]) => {
+        this.posts = posts;
+      }
+    );
+  }
+
+  onDelete(postId: string) {
+    this.postsService.deletePost(postId);
+  }
+
+  ngOnDestroy(): void {
+    this.postSubcription.unsubscribe()
+  }
+}
