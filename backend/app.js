53cbb6dc81a76118edaa2ce5a1f54c6b281caa12
Brant Hajek <branthajek@gmail.com>
2025-08-08 14:42:45 -0700

    add dotenv and update db connection

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,19 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const path = require('path');
+const username = process.env.MONGO_DB_USERNAME;
+const password = process.env.MONGO_DB_PASSWORD;
+const cluster = process.env.MONGO_DB_CLUSTER;
+const dbname = process.env.MONGO_DB_NAME;
+const uri = `mongodb+srv://${username}:${password}@${cluster}/${dbname}?retryWrites=true&w=majority`;
 
 const postsRoutes = require('./routes/posts');
 
 const app = express();
 
-mongoose.connect("mongodb+srv://brant:wH1IOSB2jCBeZdTO@cluster0.yaes6gz.mongodb.net/node-angular?retryWrites=true&w=majority")
+mongoose.connect(uri)
   .then(() => {
     console.log('Connected to database!');
   })
